627485525be10023fb4b6c6db1fdb103b1853e8b
soraweew <sorawee.wata@gmail.com>
2025-09-16 17:00:42 +0700

    Add delete book

--- src/main/java/dev/soraweew/books/controller/BookController.java
+++ src/main/java/dev/soraweew/books/controller/BookController.java
@@ -61,4 +61,9 @@ public class BookController {
             }
         }
     }
+
+    @DeleteMapping("/api/books/{title}")
+    public void deleteBook(@PathVariable String title) {
+        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
+    }
 }
