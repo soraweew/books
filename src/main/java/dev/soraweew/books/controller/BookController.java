0976a17c21af2d70de5f1922e4e5d737a53d9217
soraweew <sorawee.wata@gmail.com>
2025-09-16 15:26:10 +0700

    Initial commit

---
+++ src/main/java/dev/soraweew/books/controller/BookController.java
@@ -0,0 +1,51 @@
+package dev.soraweew.books.controller;
+
+import dev.soraweew.books.entity.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title one", "Author one", "science"),
+                new Book("Title two", "Author two", "science"),
+                new Book("Title three", "Author three", "history"),
+                new Book("Title four", "Author four", "math"),
+                new Book("Title five", "Author five", "math"),
+                new Book("Title six", "Author six", "math")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @GetMapping("/api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title) {
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBookByCategory(@RequestParam String category) {
+        return books.stream()
+                .filter(book -> book.getCategory().equalsIgnoreCase(category))
+                .toList();
+    }
+}
